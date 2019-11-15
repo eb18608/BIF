339f5dd13aa46f63fb9e672796b6176770c7fce7
Skyrah1 <zhunchenyek1@gmail.com>
2019-11-15 16:39:41 +0000

    Created basic app
    It is VERY barebones at the moment, and I still need to set it up so that it'll run what's in the Placeholder class to make sure it works with Processing, but once I've done that we should be good to go

---
+++ androidAndUi/app/src/main/java/com/bioinspiredflight/Placeholder.java
@@ -0,0 +1,25 @@
+package com.bioinspiredflight;
+
+import processing.core.PApplet;
+
+/**
+ * This is a placeholder class for Processing code.
+ * The game is going to run from here.
+ */
+public class Placeholder extends PApplet{
+
+    public void settings() {
+        size(600, 600);
+    }
+
+    public void setup() {
+
+    }
+
+    public void draw() {
+        if (mousePressed) {
+            ellipse(mouseX, mouseY, 50, 50);
+        }
+    }
+
+}
