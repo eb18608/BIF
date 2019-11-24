b61c2f51dabf183e0f51e91fd54fb34daa8929c4
Skyrah1 <zhunchenyek1@gmail.com>
2019-11-24 17:29:23 +0000

    I'll ask about the library for 3d vectors later, but for now I'll just have this return x and y values

---
+++ androidAndUi/app/src/main/java/com/bioinspiredflight/JoystickToVector3D.java
@@ -0,0 +1,18 @@
+package com.bioinspiredflight;
+
+public class JoystickToVector3D {
+
+    final private Joystick joystick;
+    final private int x, y;
+    //final private Vector3d
+
+    public JoystickToVector3D(Joystick joystick){
+        this.joystick = joystick;
+        this.x = 0;
+        this.y = 0;
+    }
+
+
+
+
+}
