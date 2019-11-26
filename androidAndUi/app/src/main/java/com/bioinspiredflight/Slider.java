0cbe5c9207834f809079181ef6756dcea9fb88fc
Skyrah1 <zhunchenyek1@gmail.com>
2019-11-26 11:55:52 +0000

    Create Slider.java

---
+++ androidAndUi/app/src/main/java/com/bioinspiredflight/Slider.java
@@ -0,0 +1,48 @@
+package com.bioinspiredflight;
+
+import android.content.Context;
+import android.graphics.PixelFormat;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+
+public class Slider extends SurfaceView implements SurfaceHolder.Callback, View.OnTouchListener {
+
+    private float length;
+    private float sliderRadius;
+
+    public Slider(Context context) {
+        super(context);
+        getHolder().addCallback(this);
+        setOnTouchListener(this);
+        this.setZOrderOnTop(true);
+        this.getHolder()
+                .setFormat(PixelFormat.TRANSPARENT);
+    }
+
+    private void setup(){
+        length = getY() / 2;
+        sliderRadius = getX() / 10;
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        return false;
+    }
+}
