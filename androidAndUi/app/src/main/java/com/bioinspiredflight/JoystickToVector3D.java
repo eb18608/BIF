985897b5a27988867970857638e268b24fa12c04
Skyrah1 <zhunchenyek1@gmail.com>
2019-11-26 11:41:12 +0000

    JoystickToVector3D now ACTUALLY has a 3d vector in it

--- androidAndUi/app/src/main/java/com/bioinspiredflight/JoystickToVector3D.java
+++ androidAndUi/app/src/main/java/com/bioinspiredflight/JoystickToVector3D.java
@@ -1,12 +1,15 @@
 package com.bioinspiredflight;
 
+import javax.vecmath.Vector3d;
+
 public class JoystickToVector3D implements Joystick.JoystickListener {
 
     //final private Joystick joystick;
     //final private float x, y;
-    //final private Vector3d
+    final private Vector3d vector3d;
 
     public JoystickToVector3D(){
+        vector3d = new Vector3d();
         //this.joystick = joystick;
         //this.x = 0;
         //this.y = 0;
@@ -14,7 +17,15 @@ public class JoystickToVector3D implements Joystick.JoystickListener {
 
     @Override
     public void onJoystickMoved(float xPercent, float yPercent, int id){
-        System.out.printf("Joystick X: %.3f, Y: %.3f\n", xPercent, yPercent);
+        //System.out.printf("Joystick X: %.3f, Y: %.3f\n", xPercent, yPercent);
+        vector3d.setX(xPercent);
+        vector3d.setY(-yPercent);
+        vector3d.setZ(0);
+        System.out.println(vector3d.toString());
+    }
+
+    public Vector3d getVector(){
+        return vector3d;
     }
 
 }
