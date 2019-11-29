f62c3d8f54da821ef7b050b92119b931b8fa82f5
Skyrah1 <zhunchenyek1@gmail.com>
2019-11-29 12:50:41 +0000

    Refactored some code in Joystick and made Slider look better

--- androidAndUi/app/src/main/java/com/bioinspiredflight/Slider.java
+++ androidAndUi/app/src/main/java/com/bioinspiredflight/Slider.java
@@ -6,6 +6,7 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.PixelFormat;
 import android.graphics.PorterDuff;
+import android.graphics.Rect;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
@@ -17,6 +18,7 @@ public class Slider extends SurfaceView implements SurfaceHolder.Callback, View.
     private float sliderRadius;
     private float centerX;
     private float centerY;
+    private int left, top, right, bottom;
 
     public Slider(Context context) {
         super(context);
@@ -28,10 +30,19 @@ public class Slider extends SurfaceView implements SurfaceHolder.Callback, View.
     }
 
     private void setup(){
-        length = getHeight() / 2f;
-        sliderRadius = Math.min(getWidth(), getHeight()) / 15f;
-        centerX = getWidth() / 7f;
-        centerY = getHeight() / 2f;
+        float h = getHeight();
+        float w = getWidth();
+        length = h / 2f;
+        sliderRadius = Math.min(w, h) / 15f;
+        centerX = w / 7f;
+        centerY = h - (h / 3.2f);
+
+        float sliderWidthFromCenter = w / 40f;
+        float sliderHeightFromCenter = h / 6.5f;
+        left = (int) (centerX - sliderWidthFromCenter);
+        top = (int) (centerY - sliderHeightFromCenter);
+        right = (int) (centerX + sliderWidthFromCenter);
+        bottom = (int) (centerY + sliderHeightFromCenter);
     }
 
     private void drawSlider(float x, float y){
@@ -40,6 +51,8 @@ public class Slider extends SurfaceView implements SurfaceHolder.Callback, View.
             Canvas canvas = this.getHolder().lockCanvas();
             Paint colors  = new Paint();
             canvas.drawColor(Color.TRANSPARENT, PorterDuff.Mode.CLEAR);
+            colors.setARGB(255, 50, 50, 50);
+            canvas.drawRect(new Rect(left, top, right, bottom), colors);
             //colors.setARGB(255, 50, 50, 50);
             colors.setARGB(255, 0, 0xdb, 0xff);
             canvas.drawCircle(x, y, sliderRadius, colors);
