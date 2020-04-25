c939493e336f5a10b8e187bcd9ee12949b685765
eb18608 <eb18608@bristol.ac.uk>
2020-04-25 15:26:33 +0100

    Streams are bigger and loops last longer

--- androidAndUi/app/src/main/java/com/bioinspiredflight/gameobjects/AirStreamObject.java
+++ androidAndUi/app/src/main/java/com/bioinspiredflight/gameobjects/AirStreamObject.java
@@ -14,7 +14,7 @@ public class AirStreamObject extends GameObject {
     public AirStreamObject(GameSketch sketch, PShape body, float x, float y, float z,
                            float scale, float dirY, int id, float dirX, float dirZ) {
         super(sketch, body, x, y, z, id);
-        h = 2000 * scale;
+        h = 4000 * scale;
         w = 200 * scale;
         d = 200 * scale;
         direction.y = dirY;
